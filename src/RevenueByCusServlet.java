7c612194cdedb8528ad0c034f6aefa7114ad68a3
Gatsby Tang <349641378@qq.com>
2018-05-02 13:51:19 -0400

    fix a bug

--- src/RevenueByCusServlet.java
+++ src/RevenueByCusServlet.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import ListOfDateServlet2.SaleReportInfo;
 
 /**
  * Servlet implementation class RevenueByCusServlet
