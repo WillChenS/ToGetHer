20f3d117405f5a3a229e0a0157df15c2905cccd3
WillChenS <32554472+WillChenS@users.noreply.github.com>
2018-05-02 21:54:36 -0400

    Manager:  Editing Employee Info Done
    Complete.

---
+++ WebContent/alterEmployeeInfo.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Add, Edit, or Delete Employee Info</title>
+</head>
+<body>
+
+<h1>Edit Employee Info</h1>
+
+	<form action="EditEmployeeServlet" method="post">
+        Select an employee to edit:
+        <br><br>
+        <select name="selectedEmployee">
+            <c:forEach var="employee" items="${employees}" varStatus="loop">
+                <option value="${ssn.get(loop.index)}">${employee}</option>
+            </c:forEach>
+        </select>
+        <input type="submit" value="Submit" align="middle"> 
+    </form>
+    
+    <table>
+	    <c:forEach var="attribute" items="${attributes}">
+	    	<tr><c:out value = "${attribute}"/></tr>
+	    </c:forEach>
+    </table>
+
+</body>
+</html>
---
+++ src/FinishEditEmployeeServlet.java
@@ -0,0 +1,74 @@
+
+
+import java.io.IOException;
+
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;    
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FinishEditEmployeeServlet
+ */
+@WebServlet("/FinishEditEmployeeServlet")
+public class FinishEditEmployeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FinishEditEmployeeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		String profileID = request.getParameter("profileID");
+		String selectedCol = request.getParameter("selectedCol");
+		String newValue = request.getParameter("newValue");
+		String toDelete = request.getParameter("delete");
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-mm-dd HH:mm:ss");
+		String updateTime = dtf.format(LocalDateTime.now());
+		Connection c = Database.getCon();
+		String query;
+		if (toDelete != null && toDelete.equals("DELETE")) {
+			newValue = null;
+		}
+		query = "UPDATE employee SET " + selectedCol + " = ? WHERE SSN = \"" + profileID + "\"";
+		PreparedStatement ps;
+		try {
+			ps = c.prepareStatement(query);
+			ps.setString(1, newValue);
+			ps.executeUpdate();
+			request.setAttribute("returnStatement", "Successfully updated.");
+			request.getRequestDispatcher("/EmployeeInfoUpdated.jsp").forward(request, response);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			request.setAttribute("returnStatement", "The value could not be updated.");
+			request.getRequestDispatcher("/EmployeeInfoUpdated.jsp").forward(request, response);
+			e.printStackTrace();
+		}
+	}
+
+}
